dc6584ac1101ffec4ea853b3d173120f178816e1
Tyler <tyler.j.buell@wmich.edu>
2019-01-10 17:47:15 -0500

    2nd commit, Insert and update functions have been completed to sqlite db file

--- TicketingSystem.py
+++ TicketingSystem.py
@@ -1,7 +1,8 @@
 from termcolor import colored
 import sys
-from sqlalchemy import *
-# import DBPractice
+import DatabaseHandler
+
+
 
 
 
@@ -9,14 +10,14 @@ from sqlalchemy import *
 class User:
 
     #initializing DB
-    db = create_engine("sqlite:///TicketDB.db")
-    metadata = MetaData(db)
-    UserDB = Table("User", metadata, autoload=True)
-
-    #DB functions
-    UserDB_insert = UserDB.insert()
-    UserDB_update = UserDB.update()
-    UserDB_delete = UserDB.delete()
+    # db = create_engine("sqlite:///TicketDB.db")
+    # metadata = MetaData(db)
+    # UserDB = Table("User", metadata, autoload=True)
+    #
+    # #DB functions
+    # UserDB_insert = UserDB.insert()
+    # UserDB_update = UserDB.update()
+    # UserDB_delete = UserDB.delete()
 
     user_count = 0
     workgroups = ["Support Center", "Analyst"]
@@ -29,39 +30,61 @@ class User:
     logged_in = False
 
 
-    def __init__(self, username, workgroup):
+    def __init__(self,username, workgroup):
         User.user_count += 1
+        self.ID = User.user_count
         self.user_count = User.user_count
         self.username = username
         self.workgroup = workgroup
         self.assigned_tickets = []
 
+        #inserting into Database
+        DatabaseHandler.insert(self)
+
     def assign_ticket(self):
         user_dict = User.user_dict
         ticket = ticket_select()
-        previous_owner = ticket.user_assigned_to
+        previous_owner = ticket.assigned_user
         if ticket.resolved:
             print(colored("\n**Ticket# {} for {} is resolved and therefore cannot be assigned**".format(ticket.ID,ticket.name),"red"))
             input("[Enter to continue to menu]")
             menu()
         user = user_select()
         ticket.user_assigned_to = user.username
+        ticket.assigned_user = user
         ticket.workgroup_assigned_to = user.workgroup
+
+        # updating assigned tickets list when a ticket is passed from one user to another
         if ticket.ID in self.assigned_tickets:
             self.assigned_tickets.remove(ticket.ID)
             user.assigned_tickets.append(ticket.ID)
+        elif ticket.ID in previous_owner.assigned_tickets:
+            previous_owner.assigned_tickets.remove(ticket.ID)
+            user.assigned_tickets.append(ticket.ID)
         else:
             user.assigned_tickets.append(ticket.ID)
+        # Updating user database assigned tickets
+        DatabaseHandler.update_user(user, assigned_tickets=True)
+        DatabaseHandler.update_user(previous_owner, assigned_tickets= True)
         user_dict["assigned_tickets"][user.username] = ticket
 
-        User.UserDB_update.where(User.UserDB.c.username == ticket.user_assigned_to).\
-            values(assigned_tickets = str(user.assigned_tickets)).execute()
+        # Updating ticket database assigned user and workgroup
+        DatabaseHandler.update_ticket(ticket,Assigned_User= True)
+        DatabaseHandler.update_ticket(ticket, Workgroup= True)
 
-        User.UserDB_update.where(User.UserDB.c.username == previous_owner). \
-                 values(assigned_tickets = str(self.assigned_tickets)).execute()
+
+        # Updating Databases after assignment of ticket
+        # User.UserDB_update.where(User.UserDB.c.username == ticket.user_assigned_to).\
+        #     values(assigned_tickets = str(user.assigned_tickets)).execute()
+        #
+        # User.UserDB_update.where(User.UserDB.c.username == previous_owner). \
+        #          values(assigned_tickets = str(self.assigned_tickets)).execute()
+        #
+        # Ticket.TicketDB_update.where(Ticket.TicketDB.c.ID == ticket.ID). \
+        #     values(Assigned_User = ticket.user_assigned_to)
 
         print(colored("\n**Ticket# {} assigned to User: {} in the {} workgroup**".format(ticket.ID, user.username,
-                                                                                 user.workgroup),"blue"))
+                                                                               user.workgroup),"blue"))
         input("[Enter to continue to menu]")
 
 
@@ -80,10 +103,6 @@ class User:
 
 
 def create_user():
-
-
-
-
     space = "\n" * 10
     print(space + "Create User Account:\n-------------------")
     username = input("Create Username: ")
@@ -93,15 +112,10 @@ def create_user():
         workgroup = input("Select Workgroup: ")
 
     user = User(username, workgroup)
-
-
     print(colored("\n**User: {} created successfully**".format(user.username),"blue"))
     input("[Enter to continue...]")
     User.user_dict["username"][user.username] = user
     User.user_dict["workgroup"][user.username] = user
-    #User.user_dict["assigned_tickets"][user.assigned_tickets] = user
-
-    User.UserDB_insert.execute({"username":user.username,"workgroup":user.workgroup,"assigned_tickets":str(user.assigned_tickets)})
     menu()
     return user
 
@@ -137,6 +151,16 @@ def user_login():
 
 
 class Ticket:
+    # initializing DB
+    # db = create_engine("sqlite:///TicketDB.db")
+    # metadata = MetaData(db)
+    # TicketDB = Table("Ticket", metadata, autoload=True)
+    #
+    # # DB functions
+    # TicketDB_insert = TicketDB.insert()
+    # TicketDB_update = TicketDB.update()
+    # TicketDB_delete = TicketDB.delete()
+
     created_tickets = 0
     tick_dict = {"ID": {}, "name": {}, "creator": {}}
     name_tickets = []
@@ -156,9 +180,20 @@ class Ticket:
         self.created_tickets = Ticket.created_tickets
         self.workgroup_assigned_to = User.logged_in_workgroup
         self.user_assigned_to = User.logged_in_username
+        self.assigned_user = User.logged_in_user
         self.resolution_note = ""
         self.status = "Open"
         self.resolved = False
+        # Inserting into Database
+        DatabaseHandler.insert(self)
+        #Inserting Ticket into Database
+        # Ticket.TicketDB_insert.execute({"ID": self.ID, "Creator": self.creator,
+        #                                 "Name": self.name, "Callback": self.callback,
+        #                                 "Location": self.location, "Summary": self.summary,
+        #                                 "Detail": self.detail, "Assigned_User": self.user_assigned_to,
+        #                                 "Workgroup": self.workgroup_assigned_to, "Status": self.status,
+        #                                 "Resolved": self.resolved, "Resolution_Note": self.resolution_note})
+
 
     def __repr__(self):
         repres = "Ticket({},{},{},{},{},{},{})".format(self.ID, self.creator, self.name, self.callback, self.location,
@@ -191,6 +226,12 @@ class Ticket:
             self.resolved = True
             self.status = "Resolved"
             self.resolution_note = input("\nEnter Resolution Notes: ")
+
+            # Updating Database status resolved boolean and resolution notes
+            DatabaseHandler.update_ticket(self, Status=True)
+            DatabaseHandler.update_ticket(self, Resolved= True)
+            DatabaseHandler.update_ticket(self, Resolution_Note=True)
+
             print(colored("\n**Ticket# {} has been resolved**".format(self.ID),"blue"))
             input("[Enter to continue...]")
         else:
@@ -206,6 +247,13 @@ class Ticket:
             self.resolved = False
             self.status = "Open"
             self.resolution_note = ""
+
+            # Updating Database status resolved boolean and resolution notes
+            DatabaseHandler.update_ticket(self, Status=True)
+            DatabaseHandler.update_ticket(self, Resolved=True)
+            DatabaseHandler.update_ticket(self, Resolution_Note=True)
+
+
             print(colored("\n**Ticket# {} is now back Open**".format(self.ID),"blue"))
             input("[Enter to continue...]")
         else:
@@ -218,12 +266,16 @@ def generate_ticket(user):
     print(space + "Fill in ticket information:\n")
     ticket = Ticket(user.username, input("Name: "), input("Callback #: "), input("Location: ")
                     , input("Summary: "), input("Details: "))
+
     ticket.name_tickets.append(ticket)
     ticket.creator_tickets.append(ticket)
+    user.assigned_tickets.append(ticket.ID)
 
     ticket.tick_dict["ID"][ticket.ID] = ticket
     ticket.tick_dict["name"][ticket.name] = ticket.name_tickets
     ticket.tick_dict["creator"][ticket.creator] = ticket.creator_tickets
+    DatabaseHandler.update_user(user, assigned_tickets=True)
+
 
     print(colored("\n**Ticket# {} created successfully for customer name: {}**".format(ticket.ID, ticket.name),"blue"))
     input("[Enter to continue...]")
@@ -254,6 +306,7 @@ def ticket_select():
 
 
 def ticket_lookup():
+    # column_count = 0
     error = True
     space = "\n" * 10
     print(
@@ -267,14 +320,10 @@ def ticket_lookup():
             print("\nValid IDs: {}".format(valid_ids))
             ID = input("Enter ID: ")
 
-            try:
-                while ID == "" or int(ID) not in valid_ids:
-                    ID = input("Enter ID: ")
-                ticket = Ticket.tick_dict[search][int(ID)]
-            except ValueError:
-                print(colored("\n**Invalid Input Type, try again**"),"red")
-                input("[Enter to continue...]")
-                menu()
+
+            while ID == "" or int(ID) not in valid_ids:
+                ID = input("Enter ID: ")
+            ticket = Ticket.tick_dict[search][int(ID)]
             print(ticket)
             input("[Enter to continue...]")
             menu()
@@ -369,12 +418,16 @@ def menu():
             user_login()
             # test ticket
             ticket = Ticket(User.logged_in_username, "John Wilson", "341-6330", "ER", "Test", "Test")
+            User.logged_in_user.assigned_tickets.append(ticket.ID)
+            DatabaseHandler.update_user(User.logged_in_user, assigned_tickets=True)
             ticket.name_tickets.append(ticket)
             ticket.creator_tickets.append(ticket)
             ticket.tick_dict["ID"][ticket.ID] = ticket
             ticket.tick_dict["name"][ticket.name] = ticket.name_tickets
             ticket.tick_dict["creator"][ticket.creator] = ticket.creator_tickets
         elif action == "1" and User.user_count < 1:
+            print(colored("**Please Create a User First**", "red"))
+            input("[Enter to continue...]")
             create_user()
         if action.lower() == "c":
             create_user()
@@ -390,8 +443,11 @@ def menu():
                 if action == "5":
                     ticket = ticket_select()
                     ticket.unresolve()
-                if action == "6":
+                if action == "6" and User.user_count > 1:
                     User.logged_in_user.assign_ticket()
+                elif action == "6":
+                    print(colored("**No additional users to assign to**", "red"))
+                    input("[Enter to continue...]")
             elif action.lower() != "q" and int(action) > 2:
                 print(colored("**Please Create a Ticket First**","red"))
                 input("[Enter to continue...]")
